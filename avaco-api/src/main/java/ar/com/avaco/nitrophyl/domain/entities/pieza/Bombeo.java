83f95facd2cc8b9bfdd21455c2069b10ed22d00d
Avaco Sistemas <avacosistemas@gmail.com>
2025-05-05 10:25:16 -0300

    proceso

--- avaco-api/src/main/java/ar/com/avaco/nitrophyl/domain/entities/pieza/Bombeo.java
+++ avaco-api/src/main/java/ar/com/avaco/nitrophyl/domain/entities/pieza/Bombeo.java
@@ -26,20 +26,5 @@ public class Bombeo {
 		this.tipo = tipo;
 	}
 
-	public Integer getMin() {
-		return min;
-	}
-
-	public void setMin(Integer min) {
-		this.min = min;
-	}
-
-	public Integer getMax() {
-		return max;
-	}
-
-	public void setMax(Integer max) {
-		this.max = max;
-	}
 	
 }
